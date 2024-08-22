a55897a405fd6d9746ae99c825471621785398d0
Sejal Kaur <sejalkaur2004@gmail.com>
2024-08-22 23:53:10 +0530

    Time: 0 ms (100%), Space: 7.6 MB (8.34%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int numbits=(int)(log2(num))+1;
+        unsigned int mask=(1U << numbits)-1;
+        return (num^mask);
+    }
+};
