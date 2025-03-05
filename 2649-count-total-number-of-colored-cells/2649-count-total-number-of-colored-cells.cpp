65b89c278b759a7541ca5b8f546bfb89dea81b05
Sejal Kaur <sejalkaur2004@gmail.com>
2025-03-05 11:13:41 +0530

    Time: 0 ms (100%), Space: 7.9 MB (31.17%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        if(n == 1)return 1;
-        long long cells = 1;
-        while(n > 0){
-            cells += 4 * (n - 1);
-            n--;
-        }
-        return cells;
+        // 1 + 4(1 + 2 + 3...) = 1 + 4((n-1)(n-1+1)/2)
+        return 1 + (2 * (long long)(n - 1) * n);
     }
 };
