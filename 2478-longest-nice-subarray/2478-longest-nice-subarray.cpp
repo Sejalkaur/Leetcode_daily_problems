76f582d107fb67ecb56f1854bfa07e067de00e2e
Sejal Kaur <sejalkaur2004@gmail.com>
2025-03-18 17:17:38 +0530

    Time: 0 ms (100%), Space: 60.9 MB (64.15%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        int result = 1;
+
+        int mask = 0;
+        while(j < n){
+
+            while((mask & nums[j]) != 0){ //if not nice and want to shrink
+                mask = (mask ^ nums[i]); // take xor
+                i++;
+            }
+
+            //if nice then for checking next take or 
+            result = max(result, j - i + 1);
+            mask = (mask | nums[j]);
+            j++;
+        }
+        return result;
+    }
+};
